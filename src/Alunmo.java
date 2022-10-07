ff103367f0f9e39e237081a222351aacb8ba9223
Alejandro Font <fontalejandro0@gmail.com>
2022-10-07 20:03:27 +0200

    Crete proyect

---
+++ src/Alunmo.java
@@ -0,0 +1,29 @@
+public class Alunmo {
+    private String Name;
+    private String LasstName;
+    private int Note;
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getLasstName() {
+        return LasstName;
+    }
+
+    public void setLasstName(String lasstName) {
+        LasstName = lasstName;
+    }
+
+    public int getNote() {
+        return Note;
+    }
+
+    public void setNote(int note) {
+        Note = note;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static String NameFile = "src\\db\\Escuela.xml";
+    public static String NameFolder = "src\\db";
+
+    static FileManager fileManager = new FileManager();
+    static Scanner scanner = new Scanner(System.in);
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        boolean InMenu = true;
+
+        fileManager.checkFolder(NameFolder);
+        fileManager.checkFile(NameFile);
+
+        while(InMenu == true) {
+            System.out.println("1 | Agregar Alumno");
+            System.out.println("2 | Salir");
+            System.out.print("Ingrese una opcion: ");
+            try {
+                int option = scanner.nextInt();
+
+                switch (option) {
+                    case 1:
+                        addStudent();
+                        break;
+                    case 2:
+                        InMenu = false;
+                        break;
+                    default:
+                        System.out.println("Opcion incorrecta");
+                        break;
+                }
+            } catch (Exception e) {
+                InMenu = false;
+                System.out.println("Que hace no pongas cosas que no son numeros");
+            }
+        }
+    }
+
+    public static void addStudent() {
+        
     }
 }
